0fc9e77a01068c173b46a3b4a4f311e554f18f6d
Lukas Stranovsky <office@stranovsky.sk>
2018-12-11 21:50:13 +0100

    Add 3d graph of points

---
+++ js/3d_plotly.js
@@ -0,0 +1,62 @@
+/* JAVASCRIPT CODE GOES HERE */
+Plotly.d3.csv('./kitti/kitti.csv', function(err, rows){
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
+    }
+
+    var data = [{
+        x: unpack(rows, 'x'),
+        y: unpack(rows, 'y'),
+        z: unpack(rows, 'z'),
+        mode: 'markers',
+        type: 'scatter3d',
+        marker: {
+            color: 'rgb(23, 190, 207)',
+            size: 2
+        }}];
+    var layout = {
+        autosize: true,
+        height: 650,
+        width:850,
+        scene: {
+            aspectratio: {
+                x: 1,
+                y: 1,
+                z: 1
+            },
+            camera: {
+                center: {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                },
+                eye: {
+                    x: 1.25,
+                    y: 1.25,
+                    z: 1.25
+                },
+                up: {
+                    x: 0,
+                    y: 0,
+                    z: 1
+                }
+            },
+            xaxis: {
+                type: 'linear',
+                zeroline: false
+            },
+            yaxis: {
+                type: 'linear',
+                zeroline: false
+            },
+            zaxis: {
+                type: 'linear',
+                zeroline: false
+            }
+        },
+        title: '3d point clustering',
+    };
+
+    Plotly.newPlot('myDiv', data, layout);
+
+});
